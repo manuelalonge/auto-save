fe6004190cb2c3e695da6252afce1812f97fe4ce
Manuel Alonge <manuel.alonge@gmail.com>
2020-02-21 16:01:33 +0100

    21022020 16:01 - removed elems variable and kept autosave

--- index.js
+++ index.js
@@ -1,35 +1,32 @@
-function getEditor() {
-    var elems = document.getElementsByTagName('textarea');
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('DOM loaded');
 
-    if (elems.length <= 0) {
-        return null;
+    var key = 'key';
+    var autoSave = document.querySelector('#autoSaveText');
+
+    autoSave.addEventListener('input', () => {
+        var text = autoSave.value;
+        window.localStorage.setItem(key, text);
+    });
+
+    function restore() {
+        var saved = window.localStorage.getItem(key);
+
+        if (saved && text) {
+            text = saved;
+        }
     }
-    return elems[0];
-}
 
-var key = 'key';
-var autoSave = document.querySelector('#autoSaveText');
+    restore();
 
-autoSave.addEventListener('input', () => {
-    var editor = getEditor();
-    var text = editor.value;
-    window.localStorage.setItem(key, text);
+    var clearButton = document.querySelector('#clearButton');
+
+    clearButton.addEventListener('click', () => {
+        window.localStorage.removeItem(key);
+        text = '';
+    });
+
+
 });
 
-function restore() {
-    var saved = window.localStorage.getItem(key);
-    var editor = getEditor();
-    if (saved && editor) {
-        editor.value = saved;
-    }
-}
 
-restore();
-
-var clearButton = document.querySelector('#clearButton');
-
-clearButton.addEventListener('click', () => {
-    window.localStorage.removeItem(key);
-    var editor = getEditor();
-    editor.value = '';
-});
